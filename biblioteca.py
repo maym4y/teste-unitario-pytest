bd7fd2fc680fba3361b30abe4f4f62b5a1e22b83
Mayara Almeida <jennim.almeida@gmail.com>
2026-05-30 22:13:40 -0300

    Teste realizado e relatório em html

--- biblioteca.py
+++ biblioteca.py
@@ -1,16 +1,20 @@
 class Livro:
+    # Construtor da classe Livro
     def __init__(self, titulo):
         self.titulo = titulo
         self.disponivel = True
     
+    # Função emprestar
     def emprestar(self):
         if not self.disponivel:
-            raise Exception("Livro indisponível")
+            raise Exception("Livro indisponível") # Gera erro se livro estiver indisponível
         
-        self.disponivel = False
+        self.disponivel = False #Indisponibiliza livro emprestado
     
     def devolver(self):
+         #Gera erro se o livro já estiver disponível para evitar redundâncias
         if self.disponivel:
             raise Exception("Este livro já foi retornado")
         
+        #Disponibiliza livro
         self.disponivel = True
--- test_biblioteca.py
+++ test_biblioteca.py
@@ -1,18 +1,21 @@
 from biblioteca import Livro
 import pytest
 
+# Testa se Livro é criado corretamente
 def test_livro_criado():
     livro = Livro("Harry Potter")
 
     assert livro.titulo == "Harry Potter"
     assert livro.disponivel is True
 
+# Testa se função de empréstimo altera estado corretamente
 def test_livro_emprestado():
     livro = Livro("Narnia")
     livro.emprestar()
 
     assert livro.disponivel is False
 
+# Testa se função de empréstimo funciona corretamente para livros indisponíveis
 def test_livro_ja_emprestado():
     livro = Livro("Jogos Vorazes")
     livro.emprestar()
@@ -20,6 +23,7 @@ def test_livro_ja_emprestado():
     with pytest.raises(Exception):
         livro.emprestar()
 
+# Testa se função de devolução funciona corretamente
 def test_devolucao_correta():
     livro = Livro("Percy Jackson")
     livro.emprestar()
